85b8f5b8ae2d59f7445f738368d7299d9e5c2438
Attila Horvath <sweeptic@gmail.com>
2025-03-10 18:58:41 +0100

    014 Navigating To Dynamic Routes

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router';
+
 export default function ClientsPage() {
+  const router = useRouter();
+
+  console.log('router.pathname', router.pathname);
+  console.log('router.query', router.query);
+
   return (
     <div>
       <h1>The projects of a given client.</h1>
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+
 export default function ClientsPage() {
+  const clients = [
+    { id: 'Max', name: 'Max' },
+    { id: 'Manuel', name: 'Manuel' },
+  ];
+
+  const LinkList = clients.map((item) => (
+    <li key={item.id}>
+      <Link href={`/clients/${item.name}`}>{`${item.name} page`}</Link>
+    </li>
+  ));
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>{LinkList}</ul>
     </div>
   );
 }
